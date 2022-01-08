f0e85d4aaa7b3602eafe89602e1b4dff79fe2816
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-01-08 20:01:00 -0300

    created function ListAccount

--- src/db/list-accounts.go
+++ src/db/list-accounts.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"fmt"
+
 	"github.com/rodolfoalvesg/api-banking/api/src/models"
 )
 
@@ -8,4 +10,14 @@ var BaseAccounts []models.Account
 
 func CreatedAccount(account models.Account) {
 	BaseAccounts = append(BaseAccounts, account)
+	fmt.Println(BaseAccounts)
+}
+
+func ListAccount(id string) (models.Account, error) {
+	for _, account := range BaseAccounts {
+		if id == account.Id {
+			return account, nil
+		}
+	}
+	return models.Account{}, fmt.Errorf("Não encontrado")
 }
