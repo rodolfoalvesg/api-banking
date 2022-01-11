74e460fa967994605940e186ed2407df2a3ce68a
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-01-11 09:11:35 -0300

    update and finished function Login

--- src/controllers/account-login.go
+++ src/controllers/account-login.go
@@ -5,8 +5,10 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/rodolfoalvesg/api-banking/api/src/db"
 	"github.com/rodolfoalvesg/api-banking/api/src/models"
 	"github.com/rodolfoalvesg/api-banking/api/src/responses"
+	"github.com/rodolfoalvesg/api-banking/api/src/security"
 )
 
 // Login, cria o logon para a api
@@ -20,7 +22,27 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	var account models.Account
 	if err := json.Unmarshal(bodyRequest, &account); err != nil {
 		responses.RespondError(w, http.StatusInternalServerError, err)
+		return
 	}
 
 	defer r.Body.Close()
+
+	verifyDocument, err := db.SearchDocument(account.Cpf)
+	if err != nil {
+		responses.RespondError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	err = security.VerifyPasswd(verifyDocument.Secret, account.Secret)
+	if err != nil {
+		responses.RespondError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	token, err := security.CreateToken(verifyDocument.Id)
+	if err != nil {
+		responses.RespondError(w, http.StatusInternalServerError, err)
+	}
+
+	responses.RespondJSON(w, http.StatusOK, models.Authentication{ID: verifyDocument.Id, Token: token})
 }
