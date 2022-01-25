091ed8637723e04437b7ef9aec2d2cf33135fdfd
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-01-25 11:06:43 -0300

    reactored db functions

--- src/db/list-accounts.go
+++ src/db/list-accounts.go
@@ -6,28 +6,49 @@ import (
 	"github.com/rodolfoalvesg/api-banking/api/src/models"
 )
 
-var BaseAccounts []models.Account
+var baseAccounts []models.Account
 
-func CreatedAccount(account models.Account) {
-	BaseAccounts = append(BaseAccounts, account)
+type Database interface {
+	AddedAccount(models.Account)
+	ShowBalanceId(id string) (models.Account, error)
+	ShowAccounts() ([]models.Account, error)
+	FindDocument(cpf string) (models.Account, error)
 }
 
-func ListBalance(id string) (models.Account, error) {
-	for _, account := range BaseAccounts {
-		if id == account.Id {
+type CreateAccount struct {
+	account models.Account
+}
+
+type ListBalance struct {
+	id string
+}
+
+type ListAccounts []models.Account
+
+type SearchDocument struct {
+	cpf string
+}
+
+func (c *CreateAccount) addedAccount() {
+	baseAccounts = append(baseAccounts, c.account)
+}
+
+func (l ListBalance) showBalanceId() (models.Account, error) {
+	for _, account := range baseAccounts {
+		if l.id == account.Id {
 			return account, nil
 		}
 	}
 	return models.Account{}, fmt.Errorf("Não encontrado")
 }
 
-func ListAccounts() ([]models.Account, error) {
-	return BaseAccounts, nil
+func (l ListAccounts) showAccounts() ([]models.Account, error) {
+	return baseAccounts, nil
 }
 
-func SearchDocument(cpf string) (models.Account, error) {
-	for _, document := range BaseAccounts {
-		if cpf == document.Cpf {
+func (s SearchDocument) findDocument() (models.Account, error) {
+	for _, document := range baseAccounts {
+		if s.cpf == document.Cpf {
 			return document, nil
 		}
 	}
