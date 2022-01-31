14278d80ad0254d275a4f7344e845c5a4dc13c93
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-01-31 09:46:26 -0300

    Test created account

--- main.go
+++ main.go
@@ -6,15 +6,17 @@ import (
 	"net/http"
 
 	"github.com/rodolfoalvesg/api-banking/api/src/config"
+	"github.com/rodolfoalvesg/api-banking/api/src/db"
 	"github.com/rodolfoalvesg/api-banking/api/src/router"
 )
 
 func main() {
 	config.LoadEnv()
-
 	r := router.CreateRouters()
 
-	fmt.Printf("Escutando servidor %d\n", config.Port)
+	CreateAccount := db.FieldsToMethodsDB{}
+	CreateAccount.AddedAccount()
 
+	fmt.Printf("Escutando servidor %d\n", config.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
