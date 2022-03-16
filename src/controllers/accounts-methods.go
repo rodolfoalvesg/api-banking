013d5a36d526c919a63ce871865f2892fcd4b18c
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-03-16 16:47:33 -0300

    refactor: business logic removided for entities/accounts and started test in /controllers

--- src/controllers/accounts-methods.go
+++ src/controllers/accounts-methods.go
@@ -1,54 +1,24 @@
 package controllers
 
 import (
-	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"time"
 
-	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/rodolfoalvesg/api-banking/api/src/db"
-	"github.com/rodolfoalvesg/api-banking/api/src/models"
+	"github.com/rodolfoalvesg/api-banking/api/src/entities/accounts"
 	"github.com/rodolfoalvesg/api-banking/api/src/responses"
-	"github.com/rodolfoalvesg/api-banking/api/src/security"
 )
 
 // CreateAccount cria uma conta
 func (c *Controller) HandlerCreateAccount(w http.ResponseWriter, r *http.Request) {
 	bodyRequest, err := ioutil.ReadAll(r.Body)
+	data, err := accounts.CreateAccount(bodyRequest, err)
 	if err != nil {
-		responses.RespondError(w, http.StatusUnprocessableEntity, err)
-		return
-	}
-
-	var account models.Account
-	if err := json.Unmarshal(bodyRequest, &account); err != nil {
-		responses.RespondError(w, http.StatusBadRequest, err)
-		return
+		responses.RespondJSON(w, http.StatusOK, err)
 	}
 
 	defer r.Body.Close()
-
-	passwdHash, err := security.SecurityHash(account.Secret) //Cria um hash da senha passada
-	if err != nil {
-		responses.RespondError(w, http.StatusBadRequest, err)
-	}
-
-	account.Secret = string(passwdHash)  //account.Secret, atribui ao campo de senha do modelo o HASH
-	account.Id = uuid.New().String()     //account.Id, cria um id único e atribui ao campo Id
-	account.CreatedAt = time.Now().UTC() //account.CreatedAt, data e hora
-	modelAccount := &db.FieldsToMethodsDB{
-		Accounts: account,
-	}
-
-	modelAccount.AddedAccount()
-
-	modelList := &db.FieldsToMethodsDB{}
-	data, err := modelList.ShowAccounts()
-	if err != nil {
-		responses.RespondError(w, http.StatusBadRequest, err)
-	}
 
 	responses.RespondJSON(w, http.StatusOK, data)
 }
--- src/controllers/accounts-methods_test.go
+++ src/controllers/accounts-methods_test.go
@@ -1,19 +1,62 @@
 package controllers
 
 import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/rodolfoalvesg/api-banking/api/src/models"
 )
 
-func TestHandlerAccounts(t *testing.T) {
+func TestHandlers(t *testing.T) {
 	t.Parallel()
 
-	// personAccount := models.Account{
-	// 	Id:        uuid.New().String(),
-	// 	Name:      "Rodolfo Alves",
-	// 	Cpf:       "01663248419",
-	// 	Secret:    "12345678",
-	// 	Balance:   5000,
-	// 	CreatedAt: time.Now(),
-	// }
+	personAccount := models.Account{
+		Name:    "Rodolfo Alves",
+		Cpf:     "01225465245",
+		Secret:  "12345678",
+		Balance: 5000,
+	}
+
+	controller := NewController(nil)
+
+	response := httptest.NewRecorder()
+
+	bodyBytes, err := json.Marshal(personAccount)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body := bytes.NewReader(bodyBytes)
+
+	t.Run("Test HandlerCreateAccount", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodPost, "/accounts", body)
+
+		controller.HandlerCreateAccount(response, request)
+		if response.Result().StatusCode != http.StatusOK {
+			t.Fail()
+		}
+
+	})
+
+	t.Run("Test HandlerShowBalance", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/accounts/{account_id}/balance", body)
+
+		controller.HandlerShowBalance(response, request)
+		if response.Result().StatusCode != http.StatusOK {
+			t.Fail()
+		}
+	})
+
+	t.Run("Testt HandlerShowAccounts", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodGet, "/accounts", body)
+
+		controller.HandlerShowAccounts(response, request)
+		if response.Result().StatusCode != http.StatusOK {
+			t.Fail()
+		}
+	})
 
 }
