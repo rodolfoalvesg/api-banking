1f8ca5d0af7ad3291bd73176c16b20d83901bf4e
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-03-17 10:09:12 -0300

    refactor: removed business logic from HandlerCreateAccount, HandlerShowBalance and HandlerShowAccounts handlers to entities/accounts

--- src/controllers/accounts-methods.go
+++ src/controllers/accounts-methods.go
@@ -1,19 +1,16 @@
 package controllers
 
 import (
-	"io/ioutil"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/rodolfoalvesg/api-banking/api/src/db"
 	"github.com/rodolfoalvesg/api-banking/api/src/entities/accounts"
 	"github.com/rodolfoalvesg/api-banking/api/src/responses"
 )
 
 // CreateAccount cria uma conta
 func (c *Controller) HandlerCreateAccount(w http.ResponseWriter, r *http.Request) {
-	bodyRequest, err := ioutil.ReadAll(r.Body)
-	acc, err := accounts.CreateAccount(bodyRequest, err)
+	acc, err := accounts.CreateAccount(r.Body)
 	if err != nil {
 		responses.RespondJSON(w, http.StatusBadRequest, err)
 		return
@@ -28,31 +25,23 @@ func (c *Controller) HandlerCreateAccount(w http.ResponseWriter, r *http.Request
 // ShowBalance, exibe o saldo
 func (c *Controller) HandlerShowBalance(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	accountId := params["account_id"]
-
-	modelListId := &db.FieldsToMethodsDB{
-		Id: accountId,
-	}
-
-	accountPerson, err := modelListId.ShowBalanceId()
+	accBalance, err := accounts.ShowBalance(params)
 	if err != nil {
 		responses.RespondError(w, http.StatusBadRequest, err)
+		return
 	}
 
-	responseAccount := &db.FieldsToMethodsDB{
-		Balance: accountPerson.Balance,
-	}
-
-	responses.RespondJSON(w, http.StatusOK, responseAccount.Balance)
+	responses.RespondJSON(w, http.StatusOK, accBalance)
+	return
 }
 
 // ShowAccounts, lista as contas
 func (c *Controller) HandlerShowAccounts(w http.ResponseWriter, r *http.Request) {
-	modelShowAccounts := &db.FieldsToMethodsDB{}
-	accountLits, err := modelShowAccounts.ShowAccounts()
+	accList, err := accounts.ShowListAccounts()
 	if err != nil {
 		responses.RespondError(w, http.StatusBadRequest, err)
 	}
 
-	responses.RespondJSON(w, http.StatusOK, accountLits)
+	responses.RespondJSON(w, http.StatusOK, accList)
+	return
 }
