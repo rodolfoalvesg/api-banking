fab6a5f6b76a285b47edff3cba2a093afa6c1983
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-03-17 18:44:09 -0300

    test: created and refactor test - HandlerCreateAccount

--- src/controllers/accounts-methods_test.go
+++ src/controllers/accounts-methods_test.go
@@ -10,53 +10,121 @@ import (
 	"github.com/rodolfoalvesg/api-banking/api/src/models"
 )
 
-func TestHandlers(t *testing.T) {
+// TestHandlerCreateAccount, teste do handler de criação de conta
+func TestHandlerCreateAccount(t *testing.T) {
 	t.Parallel()
 
-	personAccount := models.Account{
-		Name:    "Rodolfo Alves",
-		Cpf:     "01225465245",
-		Secret:  "12345678",
-		Balance: 5000,
+	personAccountA := models.Account{
+		Secret: "12345",
+	}
+	personAccountB := models.Account{
+		Secret: "123456794",
 	}
 
 	controller := NewController(nil)
 
-	response := httptest.NewRecorder()
+	testCreateAccount := map[string]struct {
+		personACC models.Account
+		want      int
+	}{
 
-	bodyBytes, err := json.Marshal(personAccount)
-	if err != nil {
-		t.Fatal(err)
+		"Status Bad": {personAccountA, http.StatusBadRequest},
+		"Status OK":  {personAccountB, http.StatusOK},
 	}
 
-	body := bytes.NewReader(bodyBytes)
+	for name, tt := range testCreateAccount {
+		response := httptest.NewRecorder()
 
-	t.Run("Test HandlerCreateAccount", func(t *testing.T) {
+		bodyBytes, err := json.Marshal(tt.personACC)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		body := bytes.NewReader(bodyBytes)
+
 		request := httptest.NewRequest(http.MethodPost, "/accounts", body)
 
 		controller.HandlerCreateAccount(response, request)
-		if response.Result().StatusCode != http.StatusOK {
-			t.Fail()
+
+		responseCode := response.Result().StatusCode
+
+		if responseCode != tt.want {
+			t.Errorf("%s: got %v, want %v", name, responseCode, tt.want)
 		}
-
-	})
-
-	t.Run("Test HandlerShowBalance", func(t *testing.T) {
-		request := httptest.NewRequest(http.MethodGet, "/accounts/{account_id}/balance", body)
-
-		controller.HandlerShowBalance(response, request)
-		if response.Result().StatusCode != http.StatusOK {
-			t.Fail()
-		}
-	})
-
-	t.Run("Testt HandlerShowAccounts", func(t *testing.T) {
-		request := httptest.NewRequest(http.MethodGet, "/accounts", body)
-
-		controller.HandlerShowAccounts(response, request)
-		if response.Result().StatusCode != http.StatusOK {
-			t.Fail()
-		}
-	})
+	}
 
 }
+
+// TestHandlerShowBalance, teste do handler para exibição de saldo
+// func TestHandlerShowBalance(t *testing.T) {
+// 	t.Parallel()
+
+// 	accBalanceA := models.Account{
+// 		Id: "kgdf4gf4gfdgf554gsfag4g",
+// 	}
+
+// 	controller := NewController(nil)
+
+// 	testShowBalance := map[string]struct {
+// 		accBalanceId models.Account
+// 		want         int
+// 	}{
+// 		"Status OK": {accBalanceA, http.StatusOK},
+// 		//"Status Bad": {personAccountB.Id, http.StatusBadRequest},
+// 	}
+
+// 	for name, tt := range testShowBalance {
+
+// 		bodyBytes, err := json.Marshal(tt.accBalanceId)
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+
+// 		body := bytes.NewReader(bodyBytes)
+
+// 		request := httptest.NewRequest(http.MethodGet, "/accounts/{account_id}/balance", body)
+// 		response := httptest.NewRecorder()
+
+// 		params := mux.Vars(request)
+// 		fmt.Println("Aqui", params)
+
+// 		controller.HandlerShowBalance(response, request)
+
+// 		respondeCode := response.Result().StatusCode
+
+// 		if respondeCode != tt.want {
+// 			t.Errorf("%s: got %v, want %v", name, respondeCode, tt.want)
+// 		}
+
+// 	}
+// }
+
+// TestHandlerShowAccounts, teste do handler para listagem de conta
+// func TestHandlerShowAccounts(t *testing.T) {
+// 	t.Parallel()
+
+// 	personAccount := models.Account{
+// 		Name:    "Rodolfo Alves",
+// 		Cpf:     "01225465245",
+// 		Secret:  "12345678",
+// 		Balance: 5000,
+// 	}
+
+// 	controller := NewController(nil)
+
+// 	response := httptest.NewRecorder()
+
+// 	bodyBytes, err := json.Marshal(personAccount)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+
+// 	body := bytes.NewReader(bodyBytes)
+
+// 	request := httptest.NewRequest(http.MethodGet, "/accounts", body)
+
+// 	controller.HandlerShowAccounts(response, request)
+// 	if response.Result().StatusCode != http.StatusOK {
+// 		t.Fail()
+// 	}
+// }
