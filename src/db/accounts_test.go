5da4bd45378a399dbac1cc16dee3c4c4b95493cc
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-03-24 09:05:31 -0300

    test: completed tests accounts

--- src/db/accounts_test.go
+++ src/db/accounts_test.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -8,24 +9,39 @@ import (
 )
 
 func TestShowBalanceId(t *testing.T) {
-	accFake := FieldsToMethodsDB{Accounts: models.Account{
-		Id:        "erwerwer74rwe4r1rwe4r58",
-		Name:      "Fulano de tal",
-		Cpf:       "01254565485",
-		Secret:    "dsdgfgf544",
-		Balance:   2000,
-		CreatedAt: time.Now(),
-	}}
 
-	accFake.AddedAccount()
+	t.Run("Teste: Falha", func(t *testing.T) {
+		accFakeID := FieldsToMethodsDB{}
+		_, err := accFakeID.ShowBalanceId()
 
-	accFakeID := FieldsToMethodsDB{Id: "erwerwer74rwe4r1rwe4r58"}
-	balance, _ := accFakeID.ShowBalanceId()
-	want := 2000
+		want := fmt.Errorf("Conta não localizada")
 
-	if balance.Balance != want {
-		t.Errorf("got %v, want %v", balance.Balance, want)
-	}
+		if err == want {
+			t.Errorf("got %v, want %v", err, want)
+		}
+	})
+
+	t.Run("Teste: OK", func(t *testing.T) {
+		accFake := FieldsToMethodsDB{Accounts: models.Account{
+			Id:        "erwerwer74rwe4r1rwe4r58",
+			Name:      "Fulano de tal",
+			Cpf:       "01254565485",
+			Secret:    "dsdgfgf544",
+			Balance:   2000,
+			CreatedAt: time.Now(),
+		}}
+
+		accFake.AddedAccount()
+
+		accFakeID := FieldsToMethodsDB{Id: "erwerwer74rwe4r1rwe4r58"}
+		balance, _ := accFakeID.ShowBalanceId()
+		want := 2000
+
+		if balance.Balance != want {
+			t.Errorf("got %v, want %v", balance.Balance, want)
+		}
+	})
+
 }
 
 func TestShowAccounts(t *testing.T) {
