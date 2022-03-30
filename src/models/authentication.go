089eaad33eee1fe15e1341a3b5ebc6a66aeecee9
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-03-30 15:45:36 -0300

    refact/fix: refactored to the DDD model

--- src/models/authentication.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Authentication struct {
-	ID    string `json:"id"`
-	Token string `json:"token"`
-}
--- src/router/routers/routers_test.go
+++
@@ -1,7 +0,0 @@
-package routers
-
-import "testing"
-
-func TestRouters(t *testing.T) {
-	
-}
