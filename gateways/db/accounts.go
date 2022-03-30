558647d082dfc08cc5d6d75d221facdab6ad2eaf
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-03-30 15:34:11 -0300

    refact/fix: account creation method return correction

---
+++ gateways/db/accounts.go
@@ -0,0 +1,46 @@
+package db
+
+import (
+	"errors"
+
+	"github.com/rodolfoalvesg/api-banking/api/domain/models"
+)
+
+var (
+	ErrNotFound = errors.New("Conta não localizada")
+)
+
+var baseAccounts = []models.Account{}
+
+// addedAccount, insere a conta no banco
+func (f *FieldsToMethodsDB) AddedAccount() (models.Account, error) {
+	baseAccounts = append(baseAccounts, f.Accounts)
+	return baseAccounts[len(baseAccounts)-1], nil
+}
+
+// showBalanceId, exibe o saldo da conta, pelo id.
+func (f *FieldsToMethodsDB) ShowBalanceId() (models.Account, error) {
+
+	for _, account := range baseAccounts {
+		if f.Id == account.Id {
+			return account, nil
+		}
+	}
+	return models.Account{}, ErrNotFound
+}
+
+// showAccounts, lista todas as contas
+func (f *FieldsToMethodsDB) ShowAccounts() ([]models.Account, error) {
+	return baseAccounts, nil
+}
+
+// findDocument Procurar se existe o cpf passado
+func (f *FieldsToMethodsDB) FindDocument() (models.Account, error) {
+	for _, document := range baseAccounts {
+		if f.Cpf == document.Cpf {
+			return document, nil
+		}
+	}
+
+	return models.Account{}, nil
+}
