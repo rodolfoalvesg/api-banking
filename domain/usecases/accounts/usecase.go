dfcab9c95838ca6b54e20b0fd9d8ad10db6c194c
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-04 12:16:23 -0300

    fix: added comments

--- domain/usecases/accounts/usecase.go
+++ domain/usecases/accounts/usecase.go
@@ -7,16 +7,19 @@ import (
 	"github.com/rodolfoalvesg/api-banking/api/domain/models"
 )
 
+//UsecaseToAccount, métodos das entidades
+type UsecaseToAccount interface {
+	CreateAccount(ctx context.Context, account models.Account) (models.Account, error)
+	ShowBalanceByID(ctx context.Context, accountID string) (int, error)
+	ShowListAccounts(ctx context.Context) ([]models.Account, error)
+}
+
+// UsecaseAccount, struct com campo de metódos das entidades
 type UsecaseAccount struct {
 	repository accounts.AccountRepository
 }
 
-type UsecaseToAccount interface {
-	CreateAccount(ctx context.Context, account []models.Account) ([]models.Account, error)
-	ShowBalanceID(ctx context.Context, accountID string) (int, error)
-	ShowListAccounts(ctx context.Context) ([]models.Account, error)
-}
-
+//NewUsecaseAccount, cria repositório com metódos das entidades
 func NewUsecaseAccount(acc accounts.AccountRepository) *UsecaseAccount {
 	return &UsecaseAccount{
 		repository: acc,
