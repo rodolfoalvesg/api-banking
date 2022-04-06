b5dd860d9320eebb21b03ea2ae41f5f8075d31cb
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-06 16:46:23 -0300

    refact: removed repository file; transferred repository code to account.go; modified method return; and created model Account

--- domain/entities/accounts/accounts.go
+++ domain/entities/accounts/accounts.go
@@ -5,46 +5,50 @@ import (
 	"errors"
 	"time"
 
-	"github.com/google/uuid"
-	"github.com/rodolfoalvesg/api-banking/api/domain/models"
-	"github.com/rodolfoalvesg/api-banking/api/gateways/db"
 	"github.com/rodolfoalvesg/api-banking/api/gateways/http/security"
 )
 
-var (
-	ErrInvalidPassword = errors.New("A senha nao atende aos requisitos")
-	ErrInvalidID       = errors.New("É preciso um ID válido como parâmetro")
-)
+// AccountRepository, repositórios da entidade accounts
+type AccountRepository interface {
+	CreateNewAccount(ctx context.Context, account Account) error
+}
+
+type Account struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	CPF       string    `json:"cpf"`
+	Secret    string    `json:"secret"`
+	Balance   int       `json:"balance"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+}
 
 // CreateAccount, verifica as regras da conta
-func CreateNewAccount(ctx context.Context, acc models.Account) (models.Account, error) {
+func (a Account) CreateNewAccount(ctx context.Context, account Account) error {
 
 	// Analisa se a senha atende os critérios
-	if len(acc.Secret) < 8 {
-		return models.Account{}, ErrInvalidPassword
+	if len(a.Secret) < 8 {
+		return errors.New("A senha nao atende aos requisitos")
 	}
 
 	//Cria um hash da senha passada
-	passwdHash, err := security.SecurityHash(acc.Secret)
+	passwdHash, err := security.SecurityHash(a.Secret)
 	if err != nil {
-		return models.Account{}, err
+		return errors.New("Não foi possível criar o HASH")
 	}
 
-	acc.Secret = string(passwdHash)  //account.Secret, atribui ao campo de senha do modelo o HASH
-	acc.ID = uuid.New().String()     //account.Id, cria um id único e atribui ao campo Id
-	acc.CreatedAt = time.Now().UTC() //account.CreatedAt, data e hora
+	a.Secret = string(passwdHash) //account.Secret, atribui ao campo de senha do modelo o HASH
 
-	return acc, nil
+	return nil
 }
 
-func ShowBalance(accID string) (int, error) {
+/*func (a Account) ShowBalance(accID string) (int, error) {
 
 	if len(accID) == 0 {
-		return 0, ErrInvalidID
+		return 0, errors.New("É preciso um ID válido como parâmetro")
 	}
 
-	modelListId := &db.FieldsToMethodsDB{
-		Id: accID,
+	modelListId := &db.Database{
+		ID: accID,
 	}
 
 	accountPerson, err := modelListId.ShowBalanceID()
@@ -52,18 +56,19 @@ func ShowBalance(accID string) (int, error) {
 		return 0, err
 	}
 
-	responseAccount := &db.FieldsToMethodsDB{
+	responseAccount := &db.Database{
 		Balance: accountPerson.Balance,
 	}
 
 	return responseAccount.Balance, nil
 }
 
-func ShowListAccounts() ([]models.Account, error) {
-	modelShowAccounts := &db.FieldsToMethodsDB{}
+func (a Account) ShowListAccounts() ([]Account, error) {
+	modelShowAccounts := &db.Database{}
 	accountLits, err := modelShowAccounts.ShowAccounts()
 	if err != nil {
 		return nil, err
 	}
 	return accountLits, err
 }
+*/
