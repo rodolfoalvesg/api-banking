52441b01641d6d1cf46ee45eaac929f3c8cfef4f
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-08 20:54:00 -0300

    refact: implemented layered dependency injection

--- main.go
+++ main.go
@@ -6,12 +6,20 @@ import (
 	"net/http"
 
 	"github.com/rodolfoalvesg/api-banking/api/common/config"
+	"github.com/rodolfoalvesg/api-banking/api/controllers"
+	account "github.com/rodolfoalvesg/api-banking/api/domain/usecases/accounts"
+	"github.com/rodolfoalvesg/api-banking/api/gateways/db"
 	"github.com/rodolfoalvesg/api-banking/api/gateways/http/router"
 )
 
 func main() {
 	config.LoadEnv()
-	r := router.CreateRouters()
+
+	repository := db.NewRepository()
+	usecase := account.NewUsecase(repository)
+	controller := controllers.NewController(usecase)
+	r := router.CreateRouters(controller)
+
 	fmt.Printf("Escutando servidor %d\n", config.Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
 }
