b065e94e12a2052187d3d135625bbcdebf1dfac2
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-08 20:42:15 -0300

    refact: modified function return; transferred password validator to account creation handler

--- domain/entities/accounts/accounts.go
+++ domain/entities/accounts/accounts.go
@@ -18,52 +18,18 @@ type Account struct {
 }
 
 // CreateAccount, verifica as regras da conta
-func ValidatePasswordHash(ctx context.Context, account Account) (string, error) {
+func ValidatePasswdHash(ctx context.Context, account Account) ([]byte, error) {
 
 	// Analisa se a senha atende os critérios
 	if len(account.Secret) < 8 {
-		return "", errors.New("A senha nao atende aos requisitos")
+		return nil, errors.New("A senha nao atende aos requisitos")
 	}
 
 	//Cria um hash da senha passada
 	passwdHash, err := security.SecurityHash(account.Secret)
 	if err != nil {
-		return "", errors.New("Não foi possível criar o HASH")
+		return nil, errors.New("Não foi possível criar o HASH")
 	}
 
-	account.Secret = string(passwdHash) //account.Secret, atribui ao campo de senha do modelo o HASH
-
-	return account.Secret, nil
+	return passwdHash, nil
 }
-
-/*func (a Account) ShowBalance(accID string) (int, error) {
-
-	if len(accID) == 0 {
-		return 0, errors.New("É preciso um ID válido como parâmetro")
-	}
-
-	modelListId := &db.Database{
-		ID: accID,
-	}
-
-	accountPerson, err := modelListId.ShowBalanceID()
-	if err != nil {
-		return 0, err
-	}
-
-	responseAccount := &db.Database{
-		Balance: accountPerson.Balance,
-	}
-
-	return responseAccount.Balance, nil
-}
-
-func (a Account) ShowListAccounts() ([]Account, error) {
-	modelShowAccounts := &db.Database{}
-	accountLits, err := modelShowAccounts.ShowAccounts()
-	if err != nil {
-		return nil, err
-	}
-	return accountLits, err
-}
-*/
