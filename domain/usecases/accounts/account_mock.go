5c6fbdcd688b96f67ce528a3af3cfef998022025
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-16 22:11:04 -0300

    refact: fixed interface parameters

--- domain/usecases/accounts/account_mock.go
+++ domain/usecases/accounts/account_mock.go
@@ -2,7 +2,6 @@ package account
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/rodolfoalvesg/api-banking/api/domain/entities/accounts"
@@ -17,16 +16,15 @@ type RepositoryMock interface {
 type UseCaseMock struct {
 	SaveAccount     func(accounts.Account) (uuid.UUID, error)
 	ListAllAccounts func(context.Context) ([]accounts.Account, error)
-	ListBalanceByID func(context.Context, uuid.UUID) (int, error)
+	ListBalanceByID func(uuid.UUID) (int, error)
 }
 
 func (m *UseCaseMock) CreateAccount(ctx context.Context, acc accounts.Account) (uuid.UUID, error) {
-	fmt.Println("Aqui mock")
 	return m.SaveAccount(acc)
 }
 
 func (m *UseCaseMock) ShowBalance(ctx context.Context, accID uuid.UUID) (int, error) {
-	return m.ListBalanceByID(ctx, accID)
+	return m.ListBalanceByID(accID)
 }
 
 func (m *UseCaseMock) ShowAccounts(ctx context.Context) ([]accounts.Account, error) {
