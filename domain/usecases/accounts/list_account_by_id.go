035c83584330b17efa7376b5a3995d805901e567
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-16 23:26:24 -0300

    refact: removed validate

--- domain/usecases/accounts/list_account_by_id.go
+++ domain/usecases/accounts/list_account_by_id.go
@@ -2,16 +2,11 @@ package account
 
 import (
 	"context"
-	"errors"
 
 	"github.com/google/uuid"
 )
 
 func (u Usecase) ShowBalance(ctx context.Context, accID uuid.UUID) (int, error) {
-
-	if len(accID) == 0 {
-		return 0, errors.New("É preciso um ID válido como parâmetro")
-	}
 
 	accBalance, err := u.repo.ListBalanceByID(ctx, accID)
 	if err != nil {
