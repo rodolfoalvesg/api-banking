cac2489d82fa8315ba0449c7e002758f3c99bd73
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-18 19:20:04 -0300

    feat: included methods mock

--- domain/entities/accounts/accounts_repository_mock.go
+++ domain/entities/accounts/accounts_repository_mock.go
@@ -7,9 +7,10 @@ import (
 )
 
 type AccountMock struct {
-	SaveAcc            func(Account) (uuid.UUID, error)
-	ListAllAcc         func(context.Context) ([]Account, error)
-	ListBalanceByIDAcc func(uuid.UUID) (int, error)
+	SaveAcc              func(Account) (uuid.UUID, error)
+	ListAllAcc           func(context.Context) ([]Account, error)
+	ListBalanceByIDAcc   func(uuid.UUID) (int, error)
+	ListAccountsByCPFAcc func(string) (Account, error)
 }
 
 func (m AccountMock) SaveAccount(ctx context.Context, acc Account) (uuid.UUID, error) {
@@ -22,4 +23,8 @@ func (m AccountMock) ListBalanceByID(ctx context.Context, accID uuid.UUID) (int,
 
 func (m AccountMock) ListAllAccounts(ctx context.Context) ([]Account, error) {
 	return m.ListAllAcc(ctx)
+}
+
+func (m AccountMock) ListAccountsByCPF(ctx context.Context, accCPF string) (Account, error) {
+	return m.ListAccountsByCPFAcc(accCPF)
 }
