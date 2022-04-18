f095f795c0fd2f0139d250bf5a81e0c40801c51f
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-18 19:47:05 -0300

    refact: validate login and return jwt

--- controllers/login.go
+++ controllers/login.go
@@ -1,38 +1,47 @@
 package controllers
 
-// Login, cria o logon para a api
-/*func (c *Controller) HandlerLogin(w http.ResponseWriter, r *http.Request) {
-	bodyRequest, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		responses.RespondError(w, http.StatusUnprocessableEntity, err)
-		return
-	}
+import (
+	"encoding/json"
+	"net/http"
 
-	var account accounts.Account
-	if err := json.Unmarshal(bodyRequest, &account); err != nil {
-		responses.RespondError(w, http.StatusInternalServerError, err)
+	account "github.com/rodolfoalvesg/api-banking/api/domain/usecases/accounts"
+	"github.com/rodolfoalvesg/api-banking/api/gateways/http/responses"
+)
+
+// Login, cria o logon para a api
+func (c *Controller) LoginHandler(w http.ResponseWriter, r *http.Request) {
+
+	var acc account.Login
+	if err := json.NewDecoder(r.Body).Decode(&acc); err != nil {
+		responses.RespondError(w, http.StatusBadRequest, err)
 		return
 	}
 
 	defer r.Body.Close()
 
-	modelFindDocument := db.Database{}
-	verifyDocument, err := modelFindDocument.FindDocument()
+	accountID, err := c.account.NewLogin(r.Context(), acc)
 	if err != nil {
-		responses.RespondError(w, http.StatusInternalServerError, err)
+		responses.RespondError(w, http.StatusBadRequest, err)
 		return
 	}
 
-	err = security.VerifyPasswd(verifyDocument.Secret, account.Secret)
-	if err != nil {
-		responses.RespondError(w, http.StatusInternalServerError, err)
-		return
-	}
+	// modelFindDocument := db.Database{}
+	// verifyDocument, err := modelFindDocument.FindDocument()
+	// if err != nil {
+	// 	responses.RespondError(w, http.StatusInternalServerError, err)
+	// 	return
+	// }
 
-	token, err := security.CreateToken(verifyDocument.ID)
-	if err != nil {
-		responses.RespondError(w, http.StatusInternalServerError, err)
-	}
+	// err = security.VerifyPasswd(verifyDocument.Secret, account.Secret)
+	// if err != nil {
+	// 	responses.RespondError(w, http.StatusInternalServerError, err)
+	// 	return
+	// }
 
-	responses.RespondJSON(w, http.StatusOK, models.Authentication{ID: verifyDocument.ID, Token: token})
-}*/
+	// token, err := security.CreateToken(verifyDocument.ID)
+	// if err != nil {
+	// 	responses.RespondError(w, http.StatusInternalServerError, err)
+	// }
+
+	responses.RespondJSON(w, http.StatusOK, accountID)
+}
