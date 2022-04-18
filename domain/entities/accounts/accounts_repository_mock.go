4e9571b5359678f45e3a90b115718ec04ed922d5
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-18 13:00:11 -0300

    feat: created mock file

---
+++ domain/entities/accounts/accounts_repository_mock.go
@@ -0,0 +1,25 @@
+package accounts
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type AccountMock struct {
+	SaveAcc            func(Account) (uuid.UUID, error)
+	ListAllAcc         func(context.Context) ([]Account, error)
+	ListBalanceByIDAcc func(uuid.UUID) (int, error)
+}
+
+func (m AccountMock) SaveAccount(ctx context.Context, acc Account) (uuid.UUID, error) {
+	return m.SaveAcc(acc)
+}
+
+func (m AccountMock) ListBalanceByID(ctx context.Context, accID uuid.UUID) (int, error) {
+	return m.ListBalanceByIDAcc(accID)
+}
+
+func (m AccountMock) ListAllAccounts(ctx context.Context) ([]Account, error) {
+	return m.ListAllAcc(ctx)
+}
