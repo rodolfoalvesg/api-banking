7081b25806c6b576701ad895df59a9400121ef8b
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-18 15:13:17 -0300

    tests: finished usecase tests

--- domain/usecases/accounts/create_accounts_test.go
+++ domain/usecases/accounts/create_accounts_test.go
@@ -2,7 +2,10 @@ package account
 
 import (
 	"context"
+	"errors"
+	"reflect"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/rodolfoalvesg/api-banking/api/domain/entities/accounts"
@@ -37,6 +40,16 @@ func TestCreateAccount(t *testing.T) {
 			accFake: myFakeAccount,
 			want:    accID,
 		},
+		{
+			Name: "account not created",
+			accountMock: accounts.AccountMock{
+				SaveAcc: func(accounts.Account) (uuid.UUID, error) {
+					return uuid.UUID{}, errors.New("Conflict")
+				},
+			},
+			accFake: myFakeAccount,
+			want:    uuid.UUID{},
+		},
 	}
 
 	for _, tc := range testCase {
@@ -49,8 +62,8 @@ func TestCreateAccount(t *testing.T) {
 			})
 
 			got, err := usecase.CreateAccount(context.Background(), tt.accFake)
-			if err != nil {
-				t.Errorf("Error creating account %s", err)
+			if got != tt.want && err != nil {
+				t.Errorf("Error %s", err)
 			}
 
 			if got != tt.want {
@@ -58,4 +71,129 @@ func TestCreateAccount(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestShowBalance(t *testing.T) {
+	t.Parallel()
+
+	myFakeAccount := accounts.Account{
+
+		Balance: 15500,
+	}
+
+	accID := uuid.New()
+
+	type TestCaseB struct {
+		Name        string
+		accountMock accounts.AccountMock
+		accFakeID   uuid.UUID
+		want        int
+	}
+
+	testCase := []TestCaseB{
+		{
+			Name: "Balance successfully listed",
+			accountMock: accounts.AccountMock{
+				ListBalanceByIDAcc: func(uuid.UUID) (int, error) {
+					return myFakeAccount.Balance, nil
+				},
+			},
+			accFakeID: accID,
+			want:      15500,
+		},
+		{
+			Name: "Error listing balance",
+			accountMock: accounts.AccountMock{
+				ListBalanceByIDAcc: func(uuid.UUID) (int, error) {
+					return 0, errors.New("Error")
+				},
+			},
+			accFakeID: accID,
+			want:      0,
+		},
+	}
+
+	for _, tc := range testCase {
+		tt := tc
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			usecase := NewUsecase(accounts.AccountMock{
+				ListBalanceByIDAcc: tt.accountMock.ListBalanceByIDAcc,
+			})
+
+			balance, err := usecase.ShowBalance(context.Background(), tt.accFakeID)
+			if balance != tt.want && err != nil {
+				t.Errorf("Error %s", err)
+			}
+
+		})
+	}
+}
+
+func TestShowAccounts(t *testing.T) {
+	t.Parallel()
+
+	type TestCaseC struct {
+		Name        string
+		accountMock accounts.AccountMock
+		want        []accounts.Account
+	}
+
+	myListAccounts := []accounts.Account{
+		{
+			ID:        uuid.New().String(),
+			Name:      "Rodolfo",
+			CPF:       "12345678900",
+			Secret:    "12345678",
+			Balance:   25550,
+			CreatedAt: time.Now().UTC(),
+		},
+		{
+			ID:        uuid.New().String(),
+			Name:      "Rosangela",
+			CPF:       "01472558",
+			Secret:    "12345678",
+			Balance:   50000,
+			CreatedAt: time.Now().UTC(),
+		},
+	}
+
+	testCase := []TestCaseC{
+		{
+			Name: "Accounts Listed",
+			accountMock: accounts.AccountMock{
+				ListAllAcc: func(context.Context) ([]accounts.Account, error) {
+					return myListAccounts, nil
+				},
+			},
+			want: myListAccounts,
+		},
+		{
+			Name: "Error Accounts not Listed",
+			accountMock: accounts.AccountMock{
+				ListAllAcc: func(context.Context) ([]accounts.Account, error) {
+					return nil, errors.New("Erro")
+				},
+			},
+			want: nil,
+		},
+	}
+
+	for _, tc := range testCase {
+		tt := tc
+		t.Run(tt.Name, func(t *testing.T) {
+			t.Parallel()
+
+			usecase := NewUsecase(accounts.AccountMock{
+				ListAllAcc: tt.accountMock.ListAllAcc,
+			})
+
+			listAccounts, err := usecase.ShowAccounts(context.Background())
+			if err != nil && !reflect.DeepEqual(listAccounts, tt.want) {
+			}
+
+		})
+	}
+
 }
