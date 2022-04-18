5f7e62f223aa128212046f5b524e9f207fb19ecb
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-18 19:17:56 -0300

    refact/feat: included methods mock

--- domain/usecases/accounts/account_mock.go
+++ domain/usecases/accounts/account_mock.go
@@ -11,12 +11,14 @@ type RepositoryMock interface {
 	CreateAccount(context.Context, accounts.Account) (uuid.UUID, error)
 	ShowBalance(ctx context.Context, accID uuid.UUID) (int, error)
 	ShowAccounts(ctx context.Context) ([]accounts.Account, error)
+	NewLogin(ctx context.Context, l Login) (string, error)
 }
 
 type UseCaseMock struct {
-	SaveAccount     func(accounts.Account) (uuid.UUID, error)
-	ListAllAccounts func(context.Context) ([]accounts.Account, error)
-	ListBalanceByID func(uuid.UUID) (int, error)
+	SaveAccount       func(accounts.Account) (uuid.UUID, error)
+	ListAllAccounts   func(context.Context) ([]accounts.Account, error)
+	ListBalanceByID   func(uuid.UUID) (int, error)
+	ListAccountsByCPF func(accCPF string) (accounts.Account, error)
 }
 
 func (m *UseCaseMock) CreateAccount(ctx context.Context, acc accounts.Account) (uuid.UUID, error) {
@@ -29,4 +31,8 @@ func (m *UseCaseMock) ShowBalance(ctx context.Context, accID uuid.UUID) (int, er
 
 func (m *UseCaseMock) ShowAccounts(ctx context.Context) ([]accounts.Account, error) {
 	return m.ListAllAccounts(ctx)
+}
+
+func (m *UseCaseMock) NewLogin(ctx context.Context, accCPF string) (accounts.Account, error) {
+	return m.ListAccountsByCPF(accCPF)
 }
