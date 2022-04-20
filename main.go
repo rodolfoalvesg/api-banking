962165282912465307f3e049f369cc59548a173e
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-20 18:32:14 -0300

    feat: new injection implemented

--- main.go
+++ main.go
@@ -8,16 +8,27 @@ import (
 	"github.com/rodolfoalvesg/api-banking/api/common/config"
 	"github.com/rodolfoalvesg/api-banking/api/controllers"
 	account "github.com/rodolfoalvesg/api-banking/api/domain/usecases/accounts"
-	"github.com/rodolfoalvesg/api-banking/api/gateways/db"
+	transfer "github.com/rodolfoalvesg/api-banking/api/domain/usecases/transfers"
+	account_db "github.com/rodolfoalvesg/api-banking/api/gateways/db/account"
+	transfer_db "github.com/rodolfoalvesg/api-banking/api/gateways/db/transfers"
 	"github.com/rodolfoalvesg/api-banking/api/gateways/http/router"
 )
 
 func main() {
 	config.LoadEnv()
 
-	repository := db.NewRepository()
-	usecase := account.NewUsecase(repository)
-	controller := controllers.NewController(usecase)
+	//Repositories
+	repositoryAccount := account_db.NewRepository()
+	repositoryTransfers := transfer_db.NewRepositoryTransfer()
+
+	//Usecases
+	usecaseAccount := account.NewUsecase(repositoryAccount)
+	usecaseTransfer := transfer.NewUsecaseTransfers(repositoryTransfers)
+
+	//Controllers
+	controller := controllers.NewController(usecaseAccount, usecaseTransfer)
+
+	//Router
 	r := router.CreateRouters(controller)
 
 	fmt.Printf("Listening server %d\n", config.Port)
