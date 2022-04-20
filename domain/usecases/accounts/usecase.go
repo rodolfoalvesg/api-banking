3daf39fde1a904d7e32fd2ca87e2ecb7aaa90e08
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-20 18:29:53 -0300

    refact: renamed file

---
+++ domain/usecases/accounts/usecase.go
@@ -0,0 +1,28 @@
+package account
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/rodolfoalvesg/api-banking/api/domain/entities/accounts"
+)
+
+type Repository interface {
+	SaveAccount(context.Context, accounts.Account) (uuid.UUID, error)
+	ListAllAccounts(context.Context) ([]accounts.Account, error)
+	ListBalanceByID(context.Context, uuid.UUID) (int, error)
+	ListAccountsByCPF(context.Context, string) (accounts.Account, error)
+	ListAccountByID(context.Context, string) error
+}
+
+type Usecase struct {
+	repo Repository
+}
+
+//NewUsecaseAccount, cria repositório com metódos das entidades
+func NewUsecase(repo Repository) *Usecase {
+	return &Usecase{
+		repo: repo,
+	}
+
+}
