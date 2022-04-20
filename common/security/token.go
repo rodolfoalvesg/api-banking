f0a8fca5e42c986338fc07a84f0ff022dc2d4b8a
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-19 21:44:32 -0300

    refact/feat: implemented validate token jwt

--- common/security/token.go
+++ common/security/token.go
@@ -1,6 +1,10 @@
 package security
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -24,4 +28,40 @@ func CreateToken(userId string) (string, error) {
 	}
 
 	return token, nil
+}
+
+// ValidateToken, verifica se o token passado na requisição é válido
+func ValidateToken(r *http.Request) error {
+
+	tokenString := extractToken(r)
+	token, err := jwt.Parse(tokenString, returnVerificationKey)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("Invalid token")
+}
+
+//extractToken, desmenbra o token
+func extractToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
+}
+
+//returnVerificationKey, verificação do metódo de assinatura
+func returnVerificationKey(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected subscription method! %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
