7090ac50463277f7d46745e856dc146ee73f9e14
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-22 13:12:54 -0300

    test: included method

--- domain/usecases/accounts/account_mock.go
+++ domain/usecases/accounts/account_mock.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/rodolfoalvesg/api-banking/api/domain/entities/accounts"
+	"github.com/rodolfoalvesg/api-banking/api/domain/entities/transfers"
 )
 
 type RepositoryMock interface {
@@ -12,6 +13,8 @@ type RepositoryMock interface {
 	ShowBalance(context.Context, uuid.UUID) (int, error)
 	ShowAccounts(ctx context.Context) ([]accounts.Account, error)
 	NewLogin(context.Context, Login) (string, error)
+	GetAccount(context.Context, string) (accounts.Account, error)
+	UpdateAccount(context.Context, transfers.Transfer) error
 }
 
 type UseCaseMock struct {
@@ -19,6 +22,8 @@ type UseCaseMock struct {
 	ListAllAccounts   func(context.Context) ([]accounts.Account, error)
 	ListBalanceByID   func(uuid.UUID) (int, error)
 	ListAccountsByCPF func(string) (accounts.Account, error)
+	ListAccountByID   func(string) (accounts.Account, error)
+	UpdatedAccount    func(accounts.Balance) error
 }
 
 func (m *UseCaseMock) CreateAccount(ctx context.Context, acc accounts.Account) (uuid.UUID, error) {
@@ -35,4 +40,12 @@ func (m *UseCaseMock) ShowAccounts(ctx context.Context) ([]accounts.Account, err
 
 func (m *UseCaseMock) NewLogin(ctx context.Context, l Login) (string, error) {
 	return "", nil
+}
+
+func (m *UseCaseMock) GetAccount(_ context.Context, accID string) (accounts.Account, error) {
+	return m.ListAccountByID(accID)
+}
+
+func (m *UseCaseMock) UpdateAccount(_ context.Context, transfer transfers.Transfer) error {
+	return m.UpdatedAccount(accounts.Balance{})
 }
