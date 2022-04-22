8f852af973e438c5330cc180338637d087c71a8c
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-22 13:11:20 -0300

    feat: implemented UpdateAccount

---
+++ domain/usecases/accounts/update_accounts.go
@@ -0,0 +1,54 @@
+package account
+
+import (
+	"context"
+	"errors"
+
+	"github.com/rodolfoalvesg/api-banking/api/domain/entities/accounts"
+	"github.com/rodolfoalvesg/api-banking/api/domain/entities/transfers"
+)
+
+var (
+	errInsufficientFunds = errors.New("Insufficient funds")
+)
+
+func (u Usecase) UpdateAccount(ctx context.Context, t transfers.Transfer) error {
+	originAccount, err := u.repo.ListAccountByID(ctx, t.Account_origin_ID)
+	if err != nil {
+		return err
+	}
+
+	if originAccount.Balance == 0 || originAccount.Balance < int(t.Amount) {
+		return errInsufficientFunds
+	}
+
+	destinationAccount, err := u.repo.ListAccountByID(ctx, t.Account_destination_ID)
+	if err != nil {
+		return err
+	}
+
+	originAccount.Balance -= int(t.Amount)
+	destinationAccount.Balance += int(t.Amount)
+
+	origin := accounts.Balance{
+		ID:      originAccount.ID,
+		Balance: originAccount.Balance,
+	}
+
+	err = u.repo.UpdatedAccount(ctx, origin)
+	if err != nil {
+		return err
+	}
+
+	destination := accounts.Balance{
+		ID:      destinationAccount.ID,
+		Balance: destinationAccount.Balance,
+	}
+
+	err = u.repo.UpdatedAccount(ctx, destination)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
