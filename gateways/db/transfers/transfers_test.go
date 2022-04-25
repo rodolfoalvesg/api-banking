d79fbdb8b938f6a734c0a84ef1028e124910d803
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-25 15:52:43 -0300

    test/refact: implemented table test

--- gateways/db/transfers/transfers_test.go
+++ gateways/db/transfers/transfers_test.go
@@ -2,28 +2,58 @@ package transfer_db
 
 import (
 	"context"
+	"errors"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/rodolfoalvesg/api-banking/api/domain/entities/transfers"
 )
 
 func TestSaveTransfer(t *testing.T) {
-	t.Parallel()
 
-	repository := NewRepositoryTransfer()
-
-	myTransferFake := transfers.Transfer{
-		Account_origin_ID:      uuid.New().String(),
-		Account_destination_ID: uuid.New().String(),
-		Amount:                 25400,
+	type TestCase struct {
+		name           string
+		myTransferFake transfers.Transfer
+		sourceTest     string
+		err            error
 	}
 
-	got, _ := repository.SaveTransfer(context.Background(), myTransferFake)
-	if got == (uuid.UUID{}) {
-		t.Errorf("got %v, want !=  %v", got, uuid.UUID{})
+	testCase := []TestCase{
+		{
+			name: "Save the transfer successfully",
+			myTransferFake: transfers.Transfer{
+				ID:                     uuid.New().String(),
+				Account_origin_ID:      uuid.New().String(),
+				Account_destination_ID: uuid.New().String(),
+				Amount:                 25400,
+				Created_At:             time.Now().UTC(),
+			},
+		},
+		{
+			name: "Fail if empty transfer id",
+			myTransferFake: transfers.Transfer{
+				Account_origin_ID:      uuid.New().String(),
+				Account_destination_ID: uuid.New().String(),
+				Amount:                 25400,
+				Created_At:             time.Now().UTC(),
+			},
+			err: nil,
+		},
 	}
+
+	for _, tc := range testCase {
+		tt := tc
+		t.Run(tt.name, func(t *testing.T) {
+			repository := NewRepositoryTransfer()
+			_, err := repository.SaveTransfer(context.Background(), tt.myTransferFake)
+			if !errors.Is(tt.err, err) {
+				t.Errorf("Expected %s, got %s", tt.err, err)
+			}
+		})
+	}
+
 }
 
 func TestListAllTransfers(t *testing.T) {
