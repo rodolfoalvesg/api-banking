c5fcece20dcf78d80134476e739613e481b522c5
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-25 09:41:38 -0300

    refact: fix fields

--- domain/entities/transfers/transfers_mock_repository.go
+++ domain/entities/transfers/transfers_mock_repository.go
@@ -7,14 +7,14 @@ import (
 )
 
 type TransferMock struct {
-	onSaveTransfer    func(Transfer) (uuid.UUID, error)
-	onListAllTransfer func(string) ([]Transfer, error)
+	OnSaveTransfer    func(Transfer) (uuid.UUID, error)
+	OnListAllTransfer func(string) ([]Transfer, error)
 }
 
-func (m *TransferMock) SaveTransfer(ctx context.Context, t Transfer) (uuid.UUID, error) {
-	return m.onSaveTransfer(t)
+func (m TransferMock) SaveTransfer(ctx context.Context, t Transfer) (uuid.UUID, error) {
+	return m.OnSaveTransfer(t)
 }
 
-func (m *TransferMock) ListAllTransfers(ctx context.Context, accID string) ([]Transfer, error) {
-	return m.onListAllTransfer(accID)
+func (m TransferMock) ListAllTransfers(ctx context.Context, accID string) ([]Transfer, error) {
+	return m.OnListAllTransfer(accID)
 }
