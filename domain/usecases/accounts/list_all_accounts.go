ee7f1b6cd87ba9c37b1e67752335e5de4e16584d
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-26 12:25:04 -0300

    refact: returning account without password

--- domain/usecases/accounts/list_all_accounts.go
+++ domain/usecases/accounts/list_all_accounts.go
@@ -8,9 +8,22 @@ import (
 
 // ShowAccounts, caso de uso para listar todas as contas
 func (u Usecase) ShowAccounts(ctx context.Context) ([]accounts.Account, error) {
-	accountList, err := u.repo.ListAllAccounts(ctx)
+	var accountsList []accounts.Account
+
+	accList, err := u.repo.ListAllAccounts(ctx)
 	if err != nil {
 		return []accounts.Account{}, err
 	}
-	return accountList, nil
+
+	for _, account := range accList {
+		accountsList = append(accountsList, accounts.Account{
+			ID:        account.ID,
+			Name:      account.Name,
+			CPF:       account.CPF,
+			Balance:   account.Balance,
+			CreatedAt: account.CreatedAt,
+		})
+	}
+
+	return accountsList, nil
 }
