c0e24084c95aa817bb90445be06254012ba82ac3
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-26 09:36:35 -0300

    refact: modified errors

--- controllers/accounts.go
+++ controllers/accounts.go
@@ -3,13 +3,17 @@ package controllers
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
 	"net/http"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/mux"
 	"github.com/rodolfoalvesg/api-banking/api/domain/entities/accounts"
 	"github.com/rodolfoalvesg/api-banking/api/gateways/http/responses"
+)
+
+var (
+	errInvalidPasswd = errors.New("password must be at least 8 characters long")
+	errEmptyID       = errors.New("ID cannot be empty")
 )
 
 // CreateAccountHandler, cria uma requisição para criação de conta
@@ -25,7 +29,7 @@ func (c *Controller) CreateAccountHandler(w http.ResponseWriter, r *http.Request
 
 	// Analisa se a senha atende os critérios
 	if len(acc.Secret) < 8 {
-		responses.RespondError(w, http.StatusFailedDependency, fmt.Errorf("password must be at least 8 characters long"))
+		responses.RespondError(w, http.StatusFailedDependency, errInvalidPasswd)
 		return
 	}
 
@@ -44,12 +48,13 @@ func (c *Controller) ShowBalanceHandler(w http.ResponseWriter, r *http.Request) 
 	params := mux.Vars(r)
 	accountID := params["account_id"]
 
-	if accountID == "00000000-0000-0000-0000-000000000000" {
-		responses.RespondJSON(w, http.StatusBadRequest, errors.New("ID cannot be empty."))
+	emptyID := uuid.UUID{}
+	accID := uuid.MustParse(accountID)
+
+	if accID == emptyID {
+		responses.RespondJSON(w, http.StatusBadRequest, errEmptyID)
 		return
 	}
-
-	accID := uuid.MustParse(accountID)
 
 	accBalance, err := c.account.ShowBalance(r.Context(), accID)
 	if err != nil {
