9d464a1e29f4804c06241521dbedeacae8e4f19c
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-04-27 17:45:43 -0300

    refact: renamed fileds and fix errors

--- domain/entities/transfers/transfers.go
+++ domain/entities/transfers/transfers.go
@@ -6,31 +6,31 @@ import (
 )
 
 var (
-	errInvalidLengthID = errors.New("Empty ID not allowed")
-	errInvalidAmount   = errors.New("Invalid amount, be must > 0")
-	errEqualAccounts   = errors.New("Equal source and destination accounts are not allowed.")
+	ErrInvalidLengthID = errors.New("empty ID not allowed")
+	ErrInvalidAmount   = errors.New("invalid amount, be must > 0")
+	ErrEqualAccounts   = errors.New("equal source and destination accounts are not allowed.")
 )
 
 type Transfer struct {
-	ID                     string    `json:"id"`
-	Account_origin_ID      string    `json:"account_origin_id"`
-	Account_destination_ID string    `json:"account_destination_id"`
-	Amount                 uint      `json:"amount"`
-	Created_At             time.Time `json:"created_at"`
+	ID                   string    `json:"id"`
+	AccountOriginID      string    `json:"account_origin_id"`
+	AccountDestinationID string    `json:"account_destination_id"`
+	Amount               uint      `json:"amount"`
+	Created_At           time.Time `json:"created_at"`
 }
 
 //ValidateTransferData, valida as informações na origem de uma transferência
 func ValidateTransferData(transfer *Transfer) error {
-	if len(transfer.Account_destination_ID) != 36 {
-		return errInvalidLengthID
+	if len(transfer.AccountDestinationID) != 36 {
+		return ErrInvalidLengthID
 	}
 
 	if transfer.Amount <= 0 {
-		return errInvalidAmount
+		return ErrInvalidAmount
 	}
 
-	if transfer.Account_origin_ID == transfer.Account_destination_ID {
-		return errEqualAccounts
+	if transfer.AccountOriginID == transfer.AccountDestinationID {
+		return ErrEqualAccounts
 	}
 
 	return nil
